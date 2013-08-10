0e50491848ea13eeffd7d8254db6fe34ac4b8805
Andrew Heuermann <aheuermann@gmail.com>
2013-08-10 14:00:13 -0500

    changes...

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,5 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:/usr/local/share/npm/bin:$ZSH/bin:$PATH"
+export PATH="./bin:$HOME/.rbenv/shims:/Users/a/.rvm/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:/usr/local/share/npm/bin:$ZSH/bin:/Applications/adt-bundle-mac-x86_64-20130219/sdk/tools:/Applications/adt-bundle-mac-x86_64-20130219/sdk/platform-tools:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+
